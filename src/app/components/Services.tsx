b172e4b239867e8d2b499a3c42b123467d16a1ea
anusmemon226 <anusm226@gmail.com>
2025-02-15 00:10:16 +0500

    Portfolio Enhancement

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -6,12 +6,12 @@ const Services = (_: unknown, ref: ForwardedRef<HTMLDivElement>) => {
         <div ref={ref} className='mb-10 flex flex-col items-center justify-center px-6 max-5sm:px-5 sm:px-16 md:px-10 lg:px-20'>
             <SectionHeader heading='Services' paragraph='Far far away, behind the word mountains, far from the countries Vokalia and Consonantia' />
             <div className='w-full sm:grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 sm:gap-5 py-5'>
-                <ServiceCard icon='analysis.png' title='Website Design' />
-                <ServiceCard icon='analysis.png' title='Website Design' />
-                <ServiceCard icon='analysis.png' title='Website Design' />
-                <ServiceCard icon='analysis.png' title='Website Design' />
-                <ServiceCard icon='analysis.png' title='Website Design' />
-                <ServiceCard icon='analysis.png' title='Website Design' />
+                <ServiceCard icon='analysis.webp' title='Website Design' />
+                <ServiceCard icon='analysis.webp' title='Website Design' />
+                <ServiceCard icon='analysis.webp' title='Website Design' />
+                <ServiceCard icon='analysis.webp' title='Website Design' />
+                <ServiceCard icon='analysis.webp' title='Website Design' />
+                <ServiceCard icon='analysis.webp' title='Website Design' />
             </div>
         </div>
     )
